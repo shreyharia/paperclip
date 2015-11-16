349d5ad9aa3d74095a1864c82bd4c28761a4cb85
Shreya Haria <shrey.haria@gmail.com>
2015-11-16 11:17:36 -0500

    Update thumbnail.rb

--- lib/paperclip/thumbnail.rb
+++ lib/paperclip/thumbnail.rb
@@ -78,6 +78,8 @@ module Paperclip
 
         success = convert(parameters, :source => "#{File.expand_path(src.path)}#{'[0]' unless animated?}", :dest => File.expand_path(dst.path))
       rescue Cocaine::ExitStatusError => e
+        Rails.logger.error e.message
+        Rails.logger.error e.backtrace.join("\n")
         raise Paperclip::Error, "There was an error processing the thumbnail for #{@basename}" if @whiny
       rescue Cocaine::CommandNotFoundError => e
         raise Paperclip::Errors::CommandNotFoundError.new("Could not run the `convert` command. Please install ImageMagick.")
